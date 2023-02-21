2fd87bf6328e19e99300305665a5954f9f106cd7
AtulRaj3120 <atulraj8804487622@gmail.com>
2023-02-21 13:47:02 +0530

    Created the header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,13 @@
-import './globals.css'
+import Header from "@/Components/Header";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+         <Header/> 
+        {children}
+      </body>
     </html>
-  )
+  );
 }
